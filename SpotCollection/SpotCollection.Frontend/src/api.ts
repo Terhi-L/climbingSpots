14b70671968b5ab9106e1cd2e5e28cd10cf6ba53
Salt Dev <saltdev@salt-devs-MacBook-Pro.local>
2023-03-15 21:41:42 +0100

    First data displayed on frontend

---
+++ SpotCollection/SpotCollection.Frontend/src/api.ts
@@ -0,0 +1,16 @@
+export interface ILocation {
+  id: number;
+  name: string;
+  country: string;
+  description: string;
+  recommendedBook: string;
+  image: string;
+  favoriteRoute: string;
+}
+
+export const getLocations = async () => {
+  const locations : ILocation[] = await fetch("http://localhost:5080/api/Locations")
+    .then((response) => response.json())
+    .then(data => data);
+  return locations;
+};
